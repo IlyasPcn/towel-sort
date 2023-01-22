cc8bb150f450207f535acba57e2248cf827ef766
Ilya Pcn <pchelinilya13@gmail.com>
2023-01-22 15:51:31 +1100

    feat: add func

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort (matrix) { return arguments.length > 0 ? matrix.reduce( (acc, cur, i ) =>{return acc.concat( i % 2 === 0 ? cur : cur.reverse()) }, [] ) : [] }
